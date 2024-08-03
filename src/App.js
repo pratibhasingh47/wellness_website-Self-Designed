fbae05ca228d4a4cd8ec021c096f035d22992af1
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-04 02:04:09 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import Header from './components//Header/Header';
-import './App.css'; // Make sure to update this file as well
+import React, { useState } from "react";
+import Header from "./components//Header/Header";
+import "./App.css"; // Make sure to update this file as well
 
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-  return (
-    <div className="App">
-      <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
-      <div className={`main-content ${isMenuOpen ? 'menu-open' : ''}`}>
-        {/* Your content goes here */}
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
+            <div className={`main-content ${isMenuOpen ? "menu-open" : ""}`}>
+                {/* Your content goes here */}
+            </div>
+        </div>
+    );
 }
 
 export default App;
