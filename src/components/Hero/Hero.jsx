4fcca2c776e18985351f18440404f63005aba538
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-08 01:04:24 +0530

    css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Hero.css";
 
+// Import images from the src/images directory
+import image1 from "../../images/image1.png";
+import image2 from "../../images/image2.png";
+import image3 from "../../images/image3.png";
+
 const Hero = () => {
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -12,13 +17,13 @@ const Hero = () => {
         <section className={`hero ${isLoaded ? "loaded" : ""}`}>
             <div className="hero-left">
                 <div className="circle circle1">
-                    <img src="..//images/img1.jpg" alt="Image 1" />
+                    <img src={image1} alt="Description of Image 1" />
                 </div>
                 <div className="circle circle2">
-                    <img src="..//images/img1.jpg" alt="Image 2" />
+                    <img src={image2} alt="Description of Image 2" />
                 </div>
                 <div className="circle circle3">
-                    <img src="..//images/img1.jpg" alt="Image 3" />
+                    <img src={image3} alt="Description of Image 3" />
                 </div>
             </div>
             <div className="hero-right">
