22e15748baf296a8bcca238156180dd80a7daadc
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-26 01:07:38 +0530

    jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Hero.scss";
 
-// Import images from the src/images directory
 import image1 from "../../images/img1.jpg";
 import { Link } from "react-scroll";
 
