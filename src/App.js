15665be4c3d43ab2df66e38266d138e75790ac4f
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-25 17:41:56 +0530

    home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./components//Header/Header";
 import "./App.css"; // Make sure to update this file as well
 import Hero from "./components/Hero/Hero";
+import Home from "./components/HomeSection/Home";
 
 function App() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,6 +15,7 @@ function App() {
         <div className="App">
             <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
             <Hero></Hero>
+            <Home></Home>
         </div>
     );
 }
---
+++ src/components/HomeSection/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home.css";
+
+// Import images from the src/images directory
+import image1 from "../../images/img1.jpg";
+
+const Home = () => {
+    return (
+        <section className="home">
+            <div className="home-container">
+                <div className="portrait portrait1">
+                    <img src={image1} alt="Portrait 1" />
+                </div>
+                <div className="home-content">
+                    <h2>Introduction</h2>
+                    <p>
+                        Welcome to our wellness coaching website. We believe in
+                        a holistic approach to wellness that encompasses the
+                        mind, body, and spirit. Our mission is to help you
+                        achieve optimal health and well-being through
+                        personalized coaching and support.
+                    </p>
+                </div>
+                <div className="portrait portrait2">
+                    <img src={image1} alt="Portrait 2" />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
