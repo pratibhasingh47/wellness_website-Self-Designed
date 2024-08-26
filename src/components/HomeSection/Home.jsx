5fa9c3302c282230a29b8d8cb845064e9f2f098b
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-27 00:30:27 +0530

    jsx

--- src/components/HomeSection/Home.jsx
+++ src/components/HomeSection/Home.jsx
@@ -14,13 +14,13 @@ const Home = () => {
                 <div className="home-content">
                     <h2>Introduction</h2>
                     <p>
-                        My name is Geetagya Singh. I am from Udaipur Rajasthan. I am a Wellness-Coach 
+                        My name is Geetagya Singh. I am from Udaipur Rajasthan. I am a Wellness-Coach
                         helping and guiding people towards their weight-management. I am student by background
-                        completing my graduation from Udaipur. I have keen interest in helping people to correct their 
+                        completing my graduation from Udaipur. I have keen interest in helping people to correct their
                         lifestyle. As lifestyle is the most important part of our life, it is mandatory to maintain a healthy lifestyle.
                         I am associated with Herbalife Nutrition Company. I am a independent distributor.
                         By joining with this company my lifestyle change drastically , the product provide an immense amazing result.
-                        I am so glad that I am part of these company.Our mission is to help people in 
+                        I am so glad that I am part of these company.Our mission is to help people in
                         achieving  healthy-lifestyle and well-being through
                         personalized coaching and support.
                     </p>
